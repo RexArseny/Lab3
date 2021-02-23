aebad7997d3d8ebe3e311c1a08039d647699eed4
Арсений Каплин <kaplin_arseny@mail.ru>
2021-02-23 23:18:55 +0300

    task 2 and 3 update

--- task3_dowhile/task3_dowhile.cpp
+++ task3_dowhile/task3_dowhile.cpp
@@ -1,20 +1,125 @@
-﻿// task3_dowhile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+double summ(int n)
+{
+    double result = 0;
+    int i = 0;
+    do
+    {
+        result += pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1);
+        i++;
+    } while (i < n - 1);
+    return result;
+}
+
+double summ2(double eps)
+{
+    double result = 0;
+    int i = 0;
+    do
+    {
+        result += pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1);
+        i++;
+    } while ((abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps) - 1);
+    return result;
+}
+
+void print(int n, int k)
+{
+    int c = 1;
+    int i = 0;
+    do
+    {
+        if (i == c * k)
+        {
+            c++;
+            continue;
+        }
+        else
+        {
+            cout << pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1) << endl;
+        }
+        i++;
+    } while (i < n - 1);
+}
+
+int findFirstElement(double eps)
+{
+    int i = 0;
+    do
+    {
+        i++;
+    } while ((abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps) - 1);
+    return i++;
+}
+
+int findFirstNegativeElement(double eps)
+{
+    int i = 0;
+    do
+    {
+        i++;
+    } while (((abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps) and (pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1) < 0)) - 1);
+    return i++;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+
+    int a, n, k;
+    double eps;
+    bool b = 1;
+
+    while (b == 1)
+    {
+        system("cls");
+        cout << "Выберите один из вариантов ниже:" << endl;
+        cout << "1. Задание 1" << endl;
+        cout << "2. Задание 2" << endl;
+        cout << "3. Задание 3" << endl;
+        cout << "4. Задание 4" << endl;
+        cout << "5. Задание 5" << endl;
+        cout << "6. Выход" << endl;
+        cin >> a;
+        system("cls");
+        switch (a)
+        {
+        case 1:
+            cout << "Введите n" << endl;
+            cin >> n;
+            cout << summ(n) << endl;
+            break;
+        case 2:
+            cout << "Введите eps" << endl;
+            cin >> eps;
+            cout << summ2(eps) << endl;
+            break;
+        case 3:
+            cout << "Введите n и k" << endl;
+            cin >> n;
+            cin >> k;
+            print(n, k);
+            break;
+        case 4:
+            cout << "Введите eps" << endl;
+            cin >> eps;
+            cout << findFirstElement(eps) << endl;
+            break;
+        case 5:
+            cout << "Введите eps" << endl;
+            cin >> eps;
+            cout << findFirstNegativeElement(eps) << endl;
+            break;
+        case 6:
+            b = 0;
+            break;
+        default:
+            cout << "Введено неверное значение. Повторите попытку.";
+            break;
+        }
+        system("pause");
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
