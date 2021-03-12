b3f11ccd44ff8f18689d7f1e1355b770df5557e6
Арсений Каплин <kaplin_arseny@mail.ru>
2021-03-12 18:52:42 +0300

    task3 changed 2

--- task3_dowhile/task3_dowhile.cpp
+++ task3_dowhile/task3_dowhile.cpp
@@ -21,13 +21,14 @@ double summ2(double eps)
 {
     double result = 0;
     int i = 0;
-    if ((abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps) - 1)
+    if (abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps)
     {
         do
         {
             result += pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1);
             i++;
-        } while ((abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps) - 1);
+        } while (abs(pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1)) > eps);
+        i++;
     }
     return result;
 }
